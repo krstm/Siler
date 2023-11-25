07ee7da5b4158dc4ae68c853a0841baffa0bad11
Kursat Melikoglu <kursatmelikoglu@gmail.com>
2023-11-25 12:35:42 +0300

    feat: Add XML Documentation to Constants and Methods
    Added XML summaries for improved code clarity.

--- Program.cs
+++ Program.cs
@@ -4,10 +4,30 @@ namespace Siler
 {
     internal class Program
     {
+        /// <summary>
+        /// The maximum number of concurrent tasks allowed when performing secure deletion operations.
+        /// This limits the number of files being securely deleted at the same time.
+        /// </summary>
         private static readonly int MaxConcurrentTasks = 10;
+
+        /// <summary>
+        /// The number of passes to use when securely deleting a file.
+        /// Each pass involves overwriting the file with random data to ensure its contents are irrecoverable.
+        /// </summary>
         private static readonly int SecureDeletePasses = 3;
+
+        /// <summary>
+        /// The number of passes to perform when overwriting a file during the secure deletion process.
+        /// Each pass writes a new pattern of random data over the file's contents.
+        /// </summary>
         private static readonly int OverwriteFilePasses = 3;
 
+        /// <summary>
+        /// The main entry point for the application.
+        /// It determines if the provided path is a file or a directory and performs secure deletion accordingly.
+        /// </summary>
+        /// <param name="args">Command line arguments where the first argument is expected to be the path to be securely deleted.</param>
+        /// <returns>A Task representing the asynchronous operation.</returns>
         private static async Task Main(string[] args)
         {
             string path = args.Length > 0 ? args[0] : PromptForPath();
@@ -40,12 +60,22 @@ namespace Siler
             }
         }
 
+        /// <summary>
+        /// Prompts the user to enter a path for a file or directory.
+        /// </summary>
+        /// <returns>The path entered by the user.</returns>
         private static string PromptForPath()
         {
             Console.WriteLine("Enter the path (file or directory):");
             return Console.ReadLine();
         }
 
+        /// <summary>
+        /// Asynchronously performs a secure deletion of the specified file.
+        /// This involves overwriting the file multiple times before deleting it.
+        /// </summary>
+        /// <param name="filePath">The path of the file to be securely deleted.</param>
+        /// <returns>A Task representing the asynchronous operation.</returns>
         private static async Task SecureDeleteAsync(string filePath)
         {
             try
@@ -66,6 +96,12 @@ namespace Siler
             }
         }
 
+        /// <summary>
+        /// Asynchronously overwrites a file with random data multiple times to securely delete it.
+        /// After overwriting, the file is resized, renamed, and the new path is returned.
+        /// </summary>
+        /// <param name="filePath">The path of the file to be overwritten.</param>
+        /// <returns>A Task resulting in the new file path after secure overwriting.</returns>
         private static async Task<string> OverwriteFileAsync(string filePath)
         {
             try
@@ -113,6 +149,12 @@ namespace Siler
             }
         }
 
+        /// <summary>
+        /// Calculates the buffer size for overwriting based on the file length.
+        /// Ensures that the buffer size is within the specified minimum and maximum limits.
+        /// </summary>
+        /// <param name="length">The length of the file.</param>
+        /// <returns>The calculated buffer size.</returns>
         private static int CalculateBufferSize(long length)
         {
             const int minBufferSize = 1024 * 1024;
@@ -129,6 +171,12 @@ namespace Siler
             }
         }
 
+        /// <summary>
+        /// Generates a random byte pattern of the specified size using a cryptographic random number generator.
+        /// This pattern is used for securely overwriting files.
+        /// </summary>
+        /// <param name="bufferSize">The size of the byte pattern to generate.</param>
+        /// <returns>A byte array containing the random pattern.</returns>
         private static byte[] GenerateRandomPattern(int bufferSize)
         {
             var randomBytes = new byte[bufferSize];
